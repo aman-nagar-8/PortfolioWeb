33fa7a8b649026346f44eb94b8398e3bc9c5a0a9
Aman Nagar <nagarsheetal48@gmail.com>
2025-10-06 23:38:41 +0530

    jsx fix

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/navbar";
-import Footer from "@/components/Footer";
+import Navbar from "@/components/navbar.jsx";
+import Footer from "@/components/Footer.jsx";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 
 
-import About from "@/components/About";
+import About from "@/components/About.jsx";
 
-import Skills from "../components/Skills";
+import Skills from "@/components/Skills.jsx";
 
-import Project from "@/components/Project";
-import Blogs from "@/components/Blogs";
+import Project from "@/components/Project.jsx";
+import Blogs from "@/components/Blogs.jsx";
 export default function Home() {
   return (
     <>
