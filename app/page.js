f35d8e03925b04ecd74ef1d1529f490931866030
Aman Nagar <nagarsheetal48@gmail.com>
2025-10-07 20:25:50 +0530

    components error

--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import About from "@/components/About.jsx";
 
 import Skills from "@/components/Skills.jsx";
 
-import Project from "@/components/Project.jsx";
-import Blogs from "@/components/Blogs.jsx";
+import Project from "../components/Project.jsx";
+import Blogs from "../components/Blogs.jsx";
 export default function Home() {
   return (
     <>
