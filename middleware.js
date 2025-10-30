73351f877991f9dd7ead83621ce3cc2f57aa765a
Aman Nagar <nagarsheetal48@gmail.com>
2025-10-30 21:22:00 +0530

    post req error

--- middleware.js
+++ middleware.js
@@ -4,22 +4,23 @@ import { jwtVerify } from "jose";
 const SECRET = process.env.JWT_key;
 
 export async function middleware(req) {
+   if(req.method == "POST"){
     const token = req.cookies.get("token")?.value;
 
     if(!token) NextResponse.redirect(new URL("/login" , req.url));
 
     try{
        const {payload} = await jwtVerify(token , new TextEncoder().encode(SECRET));
-       if(req.method == "POST"){
           if(payload.role == "visitor" ) return NextResponse.json({message:"you can not update it"});
-       };
-
-       return NextResponse.next();
-    }
-    catch(error){
-       console.log("token verify error in middleware : " , error);
-       return NextResponse.redirect( new URL("/login" , req.url));
-    }
+          
+          return NextResponse.next();
+         }
+         catch(error){
+            console.log("token verify error in middleware : " , error);
+            return NextResponse.redirect( new URL("/login" , req.url));
+         }
+    };
+    return NextResponse.next();
 }
 
 export const config = {
